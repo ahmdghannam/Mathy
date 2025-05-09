df2d0c13da78c27c42a7c67c62765776877caea2
Ghannam <iia7mt@gmail.com>
2025-05-09 21:43:40 +0300

    add docs and new functions

--- src/Math.kt
+++ src/Math.kt
@@ -1,4 +1,5 @@
 import kotlin.math.abs
+import kotlin.math.ln
 import kotlin.math.min
 import kotlin.math.sqrt
 
@@ -15,19 +16,16 @@ fun getSumFromAtoB(A: Int, B: Int): Int {
         getSumFromAToBMixed(A, B)
     }
 }
-
 private fun getSumFromAToBMixed(A: Int, B: Int): Int {
     if ((A > 0 && B > 0) || (A < 0 && B < 0)) throw Exception("The numbers must be Mixed!")
     val negative = if (A < 0) A else B
     val positive = if (A >= 0) A else B
     return getSumFromOneToN(positive) - getSumFromOneToN(-negative)
 }
-
 private fun getSumFromAToBNegative(A: Int, B: Int): Int {
     if (A > 0 || B > 0) throw Exception("The numbers must be Negative!")
     return -getSumFromAToBPositive(-A, -B)
 }
-
 private fun getSumFromAToBPositive(A: Int, B: Int): Int {
     if (A < 0 || B < 0) throw Exception("The numbers must be positive!")
     return min(A, B) + abs((getSumFromOneToN(A) - getSumFromOneToN(B)))
@@ -58,7 +56,6 @@ fun Int.mapRanges(oldStart: Int, oldEnd: Int, newStart: Int, newEnd: Int): Int {
     if (this !in oldStart..oldEnd) return ERROR_CODE
     return (this - oldStart) * (newEnd - newStart) / (oldEnd - oldStart) + newStart
 }
-
 fun Int.mapRanges(old: IntRange, new: IntRange): Int {
     if (this !in old) return ERROR_CODE
     return (this - old.first) * (new.last - new.first) / (old.last - old.first) + new.first
@@ -84,7 +81,6 @@ fun Number.forceInRange(start: Number, end: Number): Number {
         else -> this
     }
 }
-
 fun Long.forceInRange(range: LongRange): Long {
     return when {
         this < range.first -> range.first
@@ -130,18 +126,13 @@ fun Int.decByPercentage(percentage: Int): Int {
     }
 }
 
-/*
-*  the numbers should be separated by a comma or space and passed as string
-* */
+/**
+ *  the numbers should be separated by a comma or space and passed as string
+ * */
 fun sumNumbersInString(text: String, evenOnly: Boolean = false, oddOnly: Boolean = false): Double {
-    val textSeparatedByCommas = text.trim().replace(" ", ",", true)
-    val numbers = textSeparatedByCommas.split(",")
-        .filter { it != "" && it != " " }
-        .map { it.toDouble() }
-
-    return numbers.sumOf { value ->
-        if (evenOnly && oddOnly){
-          value
+    return convertTextItemsToArray(text).map { it.toDouble() }.sumOf { value ->
+        if (evenOnly && oddOnly) {
+            value
         } else if (evenOnly) {
             if (value.toInt().isEven()) value else 0.0
         } else if (oddOnly) {
@@ -151,29 +142,53 @@ fun sumNumbersInString(text: String, evenOnly: Boolean = false, oddOnly: Boolean
         }
     }
 }
-fun gcd(a: Int, b: Int): Int {
-    var x = a
-    var y = b
-    while (y != 0) {
-        val temp = y
-        y = x % y
-        x = temp
-    }
-    return x
+private fun convertTextItemsToArray(text: String): List<String> {
+    return text.trim()
+        .replace(" ", ",", true)
+        .split(",")
+        .filter { it != "" && it != " " }
 }
+
+/**
+ * finds the greatest common divisor of two numbers
+ **/
+tailrec fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a
+    else gcd(b, a % b)
+}
+
+/**
+ * finds the least common divisor of two numbers
+ **/
 fun lcm(a: Int, b: Int): Int {
     return if (a == 0 || b == 0) 0 else (a * b) / gcd(a, b)
 }
+
+/**
+ * finds the greatest common divisor of list of numbers
+ **/
 fun gcdList(vararg numbers: Int): Int {
     return numbers.reduce { a, b -> gcd(a, b) }
 }
+
+/**
+ * finds the least common divisor of list of numbers
+ **/
 fun lcmList(vararg numbers: Int): Int {
     return numbers.reduce { a, b -> lcm(a, b) }
 }
+
+/**
+ * finds the factorial of a number n * (n-1) * (n-2) ... 2 * 1
+ **/
 tailrec fun factorial(n: Int, acc: Long = 1): Long {
     return if (n == 0 || n == 1) acc
     else factorial(n - 1, acc * n)
 }
+
+/**
+ * Checks if the number is prime, i.e., divisible only by 1 and itself.
+ **/
 fun isPrime(n: Int): Boolean {
     if (n <= 1) return false
     for (i in 2..sqrt(n.toDouble()).toInt()) {
@@ -182,7 +197,155 @@ fun isPrime(n: Int): Boolean {
     return true
 }
 
+/**
+ * finds the fibonacci of a number
+ * fib(n) = fib(n-1) + fib(n-2)
+ *  O(n) time, O(1) space and complexities
+ **/
+fun Int.fibonacci(n: Int): Int {
+    if (n < 2) return n
+    var first = 0
+    var second = 1
+    for (i in 2..n) {
+        val temp = second
+        second += first
+        first = temp
+    }
+    return second
+}
 
+/**
+ * These pair of functions will calculate the max or min in an array,
+ *  but discards the chosen index
+ **/
+fun IntArray.maxExceptIndex(index: Int): Int {
+    var max = Int.MIN_VALUE
+    for (i in indices) {
+        if (i != index && this[i] > max) {
+            max = this[i]
+        }
+    }
+    return max
+}
+fun IntArray.minExceptIndex(index: Int): Int {
+    var min = Int.MAX_VALUE
+    for (i in indices) {
+        if (i != index && this[i] < min) {
+            min = this[i]
+        }
+    }
+    return min
+}
+
+/**
+ * Will spread the digits of a number as a list
+ *  ex 123 -> [1,2,3]
+ **/
+fun Int.spreadDigits(): List<Int> {
+    if (this == 0) return listOf(0)
+    val mutableList = mutableListOf<Int>()
+    var n = Math.abs(this)
+    while (n > 0) {
+        mutableList.add(n % 10)
+        n /= 10
+    }
+    return mutableList.reversed()
+}
+
+/**
+ * will always return a valid index to access a list of items
+ * it will consider the list as cycle
+ **/
+fun IntArray.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
+fun <T> Collection<T>.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
+fun <T> Array<T>.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
+private fun getCyclicIndex(index: Int, size: Int): Int {
+    return if (index < 0) {
+        size + index
+    } else {
+        index % size
+    }
+}
+
+/**
+ * will count the number of ones and zeros in the binary representation
+ * in a number
+ **/
+fun Int.countBinaryBits(): Pair<Int, Int> {
+    var x = this
+    var ones = 0
+    var zeros = 0
+    while (x > 0) {
+        if (x % 2 == 1) ones++
+        else zeros++
+        x /= 2
+    }
+    return Pair(zeros, ones)
+}
+
+/**
+ * returns the sum and count of digits of an integer number
+ **/
+fun Int.digitsSumAndCount(): Pair<Int, Int> {
+    var num = this
+    var sum = 0
+    var counter = 0
+    while (num > 0) {
+        sum += num % 10
+        counter++
+        num /= 10
+    }
+    return Pair(sum, counter)
+}
+
+/**
+ * will find the number of combinations of (n/r)
+ **/
+fun combinations(n: Int, r: Int): Long {
+    return factorial(n) / (factorial(r) * factorial(n - r))
+}
+
+/**
+ * finds the min and the max in array in one loop
+ **/
+fun IntArray.minMax(): Pair<Int, Int> {
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    for (num in this) {
+        if (num > max) max = num
+        if (num < min) min = num
+    }
+    return Pair(min, max)
+}
+
+/**
+ * calculates the slope of two points
+ *  point = {x,y}
+ **/
+fun calculateSlope(point1: IntArray, point2: IntArray): Double? {
+    if (point1.size < 2 || point2.size < 2) return null
+    return ((point2[1] - point1[1]) * 1.0) / (point2[0] - point1[0])
+}
+
+/**
+ * calculates the distance between two numbers in 2D
+ **/
+private fun euclideanDistance(x: Int, y: Int, queryX: Int, queryY: Int): Int {
+    return (x - queryX) * (x - queryX) + (y - queryY) * (y - queryY)
+}
+
+/**
+ * calculates the log of custom base
+ **/
+fun log(x: Int, base: Int): Int {
+    return (ln(x.toDouble()) / ln(base.toDouble())).toInt()
+}
+/**
+ * calculates the log of base 2
+ **/
+fun log2(n: Int): Int {
+    return log(n, 2)
+}
 
 
 /**
@@ -197,7 +360,7 @@ fun Int.isPositiveOrZero() = this >= 0
 fun Boolean.toInt() = if (this) 1 else 0
 fun Int.isEven() = this % 2 == 0
 fun Int.isOdd() = this % 2 == 1
-fun Int.toBoolean() = this >= 1
+fun Int.toBoolean() = abs(this) >= 1
 infix fun Double.p(power: Double) = Math.pow(this, power)
 
 const val ERROR_CODE = -1
