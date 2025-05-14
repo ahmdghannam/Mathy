4c1e8854722359f95cbc92eebc535f1d84479a72
Ghannam <iia7mt@gmail.com>
2025-05-14 21:34:06 +0300

    feat: add more array fetching techniques

--- src/arrays/arrays.kt
+++ src/arrays/arrays.kt
@@ -2,15 +2,49 @@ package arrays
 
 import numbers.gcd
 import numbers.lcm
-
+import kotlin.math.abs
 
 /**
  * will always return a valid index to access a list of items
  * it will consider the list as cycle
+ * arr[-1] -> last item
+ * arr[-size - 1] -> last item
+ * arr[size] -> first item
+ * @return valid integer index
  **/
 fun IntArray.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
 fun <T> Collection<T>.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
 fun <T> Array<T>.getCyclicIndex(index: Int): Int = getCyclicIndex(index, size)
+
+/**
+ * Returns the element at the given [index], wrapping around the array if the index is out of bounds.
+ *
+ * The index is normalized using [getCyclicIndex], so both negative and overly large indices
+ * are wrapped cyclically (i.e., like modulo operation).
+ *
+ * For example, `index = -1` returns the last element, and `index = size` returns the first element.
+ *
+ * @receiver the array from which to retrieve the element.
+ * @param index the index to access, which can be any integer (positive or negative).
+ * @return the element at the cyclically normalized index.
+ */
+fun IntArray.getElementAtCyclicIndex(index: Int): Int = getCyclicIndex(index, size).let { this[it] }
+fun <T> List<T>.getElementAtCyclicIndex(index: Int): T = getCyclicIndex(index, size).let { this[it] }
+fun <T> Array<T>.getElementAtCyclicIndex(index: Int): T = getCyclicIndex(index, size).let { this[it] }
+
+/**
+ * Returns the element at the given [index] or `null` if the index is out of bounds.
+ *
+ * Uses a safe index lookup via [getIndexOrNull], so negative indices or indices ≥ size
+ * will yield `null`.
+ *
+ * @receiver the array from which to retrieve the element.
+ * @param index the position of the element to retrieve.
+ * @return the element at `index`, or `null` if `index` is out of the array’s bounds.
+ */
+fun IntArray.getOrNull(index: Int): Int? = getIndexOrNull(index, size)?.let { this[it] }
+fun <T> List<T>.getOrNull(index: Int): T? = getIndexOrNull(index, size)?.let { this[it] }
+fun <T> Array<T>.getOrNull(index: Int): T? = getIndexOrNull(index, size)?.let { this[it] }
 
 /**
  * finds the min and the max in array in one loop
@@ -24,6 +58,7 @@ fun IntArray.minMax(): Pair<Int, Int> {
     }
     return Pair(min, max)
 }
+
 /**
  * These pair of functions will calculate the max or min in an array,
  *  but discards the chosen index
@@ -37,6 +72,7 @@ fun IntArray.maxExceptIndex(index: Int): Int {
     }
     return max
 }
+
 fun IntArray.minExceptIndex(index: Int): Int {
     var min = Int.MAX_VALUE
     for (i in indices) {
@@ -46,7 +82,6 @@ fun IntArray.minExceptIndex(index: Int): Int {
     }
     return min
 }
-
 
 /**
  * finds the greatest common divisor of list of numbers
@@ -67,8 +102,16 @@ fun lcmList(vararg numbers: Int): Int {
  **/
 private fun getCyclicIndex(index: Int, size: Int): Int {
     return if (index < 0) {
-        size + index
+        (size - (abs(index) % (size))) % size
     } else {
         index % size
+    }
+}
+
+private fun getIndexOrNull(index: Int, size: Int): Int? {
+    return if (index in 0..<size) {
+        index
+    } else {
+        null
     }
 }
